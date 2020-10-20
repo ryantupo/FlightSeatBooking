2d1b866f5dca2a3e437b896530255e4019d6a2f4
Ryan Tuplin <65490793+ryantupo@users.noreply.github.com>
2020-10-20 15:44:51 +0100

    Add files via upload

--- week2Exercise7.java
+++ week2Exercise7.java
@@ -16,28 +16,43 @@ public class week2Exercise7 {
 
     }
 
+    // main function
     public static void main(String[] args) {
 
+        //create an array of length 10
         boolean[] seats = new boolean[10];
+
+        //loop through the array and set all values to false (meaning not taken)
         for (int i = 0; i < seats.length; i++) {
             seats[i] = false;
         }
+
+        //view seats (for error detectioon)
         System.out.println(Arrays.toString(seats));
 
+        //welcome message
         System.out.println("..........Welcome to British Airways..........");
 
+        //while the whole plane isnt reserved
         while (!wholePlaneCheck(seats)) {
 
+            //a constant while loop
             while (true) {
+
+                //input - what class they would like to book into
                 Scanner obj1 = new Scanner(System.in);
                 System.out.println("Which class would you like to book?: ");
                 String classV = obj1.nextLine();
-
+                
+                //switch class to do what is needed for each class
                 switch (classV) {
-                    case "Econemy" -> classCHECK(seats, "Econemy", "First Class", 0, 4);
-                    case "First Class" -> classCHECK(seats, "First Class", "Econemy", 5, 9);
-                    default -> System.out.println("you have entered an invalid class enter [First Class or Econemy]");
-                   
+                    case "Econemy" ->
+                        classCHECK(seats, "Econemy", "First Class", 0, 4); // class Econemy is seats 1 - 5 
+                    case "First Class" ->
+                        classCHECK(seats, "First Class", "Econemy", 5, 9); // class First Class is seats 6 - 10 
+                    default -> //error detection for invalid entry
+                        System.out.println("you have entered an invalid class enter [First Class or Econemy]");
+
                 }
 
             }
@@ -49,31 +64,46 @@ public class week2Exercise7 {
     //if all plane is full say next flight in 3 mins
     //functions needed - check if seat is avaible (a seat check for all seats )
     // - check if all seats in that class are taken
+    
+    //function to check if a seat is taken - and if so book ticket 
     public static boolean[] seatCheck(boolean seats[], int start, int end, String className, String otherClass) {
+        
+        //constant while loop
         while (true) {
+            //if the class is not fully booked
             if (!classSeatCheck(seats, start, end)) {
+                //input what seat they would like to book
                 Scanner obj2 = new Scanner(System.in);
                 System.out.println("Which seat would you like to book?(): ");
                 String seatV = obj2.nextLine();
-
+                
+                //parse the input into an int
                 Integer seatN = (Integer.parseInt(seatV) - 1);
-
+                
+                //output for debugging
                 System.out.println(Arrays.toString(seats));
-
+                
+                //error detection to check number entered is in the valid options
                 if (seatN >= start && seatN <= end) {
-
+                    
+                    //if seat isnt taken
                     if (seats[seatN] == false) {
+                        //set seat to be taken
                         seats[seatN] = true;
+                        //output function to show ticket
                         System.out.println("-------------------------------------");
                         System.out.println("---------" + className + " Ticket----------");
                         System.out.println("-------------------------------------");
                         System.out.println("----------------Seat " + seatN + "---------------");
                         System.out.println("------------18:00 flight-------------");
                         System.out.println("-------------------------------------");
-
+                        
+                        //input if they would like to book another ticket?
                         Scanner obj3 = new Scanner(System.in);
                         System.out.println("Would you like to book another seat?: ");
                         String continueC = obj3.nextLine();
+                        
+                        //if anything other than yes - end program
                         if (!continueC.equals("yes")) {
                             System.exit(0);
                             break;
@@ -87,14 +117,18 @@ public class week2Exercise7 {
                 }
 
             } else {
+                //loop the code and start booking another ticket
                 classCHECK(seats, className, otherClass, start, end);
 
             }
 
         }
+        //return updated seats array
         return seats;
     }
 
+    
+    //checked if a full class is booked
     public static boolean classSeatCheck(boolean seats[], int start, int end) {
 
         boolean isFull = true;
@@ -105,9 +139,11 @@ public class week2Exercise7 {
                 System.out.println(i);
             }
         }
+        //returns boolean value if full or not
         return isFull;
     }
-
+    
+    //checks if the whole plane is booked
     public static boolean wholePlaneCheck(boolean[] seats) {
         boolean fullyBooked = true;
 
@@ -117,30 +153,40 @@ public class week2Exercise7 {
 
             }
         }
+       //returns boolean value if full or not
         return fullyBooked;
     }
-
+    
+    
+    //fucntion to ask whether they want to change class
     public static boolean changeClass(String classC, String otherClass) {
-        while (true) {
-            Scanner obj1 = new Scanner(System.in);
-            System.out.println("Sorry that all of " + classC + " seats were taken would you like to book into " + otherClass + "? ");
-            String classV = obj1.nextLine();
-            return classV.equals("yes") || classV.equals("y");
-        }
+
+        Scanner obj1 = new Scanner(System.in);
+        System.out.println("Sorry that all of " + classC + " seats were taken would you like to book into " + otherClass + "? ");
+        String classV = obj1.nextLine();
+        //returns a boolean if they want to or not
+        return classV.equals("yes") || classV.equals("y");
+
     }
-
+    
+    //main function loop for booking
     public static void classCHECK(boolean[] seats, String className, String otherClass, int start, int end) {
+        //while whole plane isnt fully booked
         if (!wholePlaneCheck(seats)) {
+            //while whole class isnt fully booked
             if (!classSeatCheck(seats, start, end)) {
-
-                seatCheck(seats, start, end, className, otherClass);
                 
+                //check if seat is taken if not book seat
+                seatCheck(seats, start, end, className, otherClass);
+
             } else {
-                System.out.println(className + " class is full would you like to book a " + otherClass + " ticket?");
+                //if class full try change class 
                 if (changeClass(className, otherClass)) {
                     if (className.equals("First Class")) {
+                        // wanna change class and used to be in first class rerun fucntion with switch varibles
                         classCHECK(seats, otherClass, className, 5, 9);
                     } else {
+                        // wanna change class and used to be in Econemy rerun fucntion with switch varibles
                         classCHECK(seats, otherClass, className, 0, 4);
                     }
                 } else {
